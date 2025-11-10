3ca62074dfa6390a27bb4bb20d686d31507638a3
OtavioEnrique <otavio.e.lopes.lima@gmail.com>
2025-11-09 23:11:43 -0300

    Opcodes 2 e 3 adicionados
    Added new opcodes for register operations and fixed assignment errors.

--- cpu.c
+++ cpu.c
@@ -7,6 +7,16 @@ void setPrograma(CPU *cpu, Instrucao *programaAux) {
     cpu->programa = programaAux;
 }
 
+/* OPCODES:
+    //-1 -> halt
+    // 0 -> soma
+    // 1 -> subtrai
+    // 2 -> copia do registrador para RAM
+    // 3 -> copia da RAM para o registrador
+    // 4 -> salva conteudo externo no registrador
+    // 5 -> obtem conteudo externo do registrador
+*/
+
 // Simula a execução das instruções da CPU
 void iniciar(CPU *cpu, RAM *ram) {
     cpu->pc = 0;
@@ -29,7 +39,22 @@ void iniciar(CPU *cpu, RAM *ram) {
             case 1: // SUBTRAÇÃO
                 setDado(ram, inst.add3, getDado(ram, inst.add1) - getDado(ram, inst.add2));
                 break;
-                
+
+            case 2:
+                if(inst.add1 = 0){
+                    setDado(ram, inst.add2, cpu->registrador1);
+                }else if(inst.add1 = 1){
+                    setDado(ram, inst.add2, cpu->registrador2);
+                }
+                break;
+
+            case 3:
+                if(inst.add1 = 0){
+                    cpu->registrador1 = getDado(ram, inst.add2);
+                }else if(inst.add1 = 1){
+                    cpu->registrador2 = getDado(ram, inst.add2);
+                }
+                break;
 
             default:
                 printf("Opcode desconhecido: %d\n", cpu->opcode);
